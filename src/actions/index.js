1e882b497c964c2c9f229fd7150965da446f4a2b
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-01-20 21:17:29 -0500

    Completed actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,36 @@ export const SMURF_FETCH_FAILURE = 'SMURF_FETCH_FAILURE';
 export const ADD_SMURF = 'ADD_SMURF';
 export const ADD_ERROR = 'ADD_ERROR';
 
+export const fetchSmurfs = () => {
+    return(dispatch) => {
+        dispatch(startSmurfFetch());
+        axios.get('http://localhost:3333/smurfs')
+            .then(resp => {
+                console.log(`Data being sent ${resp}`);
+                dispatch(smurfFetchSuccess(resp.data));
+            }).error(error => dispatch(smurfFetchFailure(error)));
+    }
+}
 
+export const startSmurfFetch = () => {
+    return({type: START_SMURF_FETCH});
+}
+
+export const smurfFetchSuccess = (smurf) => {
+    return({type: SMURF_FETCH_SUCCESS, payload: smurf});
+}
+
+export const smurfFetchFailure = (error) => {
+    return({type: SMURF_FETCH_FAILURE, payload: error});
+}
+
+export const addSmurf = (smurf) => {
+    return({type: ADD_SMURF, payload: smurf});
+}
+
+export const addError = (error) => {
+    return({type: ADD_ERROR, payload: error});
+}
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
