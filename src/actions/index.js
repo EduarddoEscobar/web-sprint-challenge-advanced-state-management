a37f29d6cc9a1e4ca5288225a1dc014e16b36823
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-01-20 21:11:45 -0500

    Completed index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+export const START_SMURF_FETCH = 'START_SMURF_FETCH';
+export const SMURF_FETCH_SUCCESS = 'SMURF_FETCH_SUCCESS';
+export const SMURF_FETCH_FAILURE = 'SMURF_FETCH_FAILURE';
+export const ADD_SMURF = 'ADD_SMURF';
+export const ADD_ERROR = 'ADD_ERROR';
+
+
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
